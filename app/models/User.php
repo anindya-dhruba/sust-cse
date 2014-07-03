ba40e3f15049f33dc4113cb64f51fee4b3f42a62
Anindya Dhruba <anindya.dhruba@gmail.com>
2014-07-03 10:33:32 +0600

    Upgrade Bug fix

--- app/models/User.php
+++ app/models/User.php
@@ -6,7 +6,7 @@ use Illuminate\Auth\Reminders\RemindableInterface;
 class User extends Eloquent implements UserInterface, RemindableInterface {
 
 	protected $table = 'users';
-	protected $hidden = array('password');
+	protected $hidden = array('password', 'remember_token');
 
 	public static $genders = array
 	(
@@ -96,6 +96,21 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	public function getReminderEmail()
 	{
 		return $this->email;
+	}
+
+	public function getRememberToken()
+	{
+	    return $this->remember_token;
+	}
+
+	public function setRememberToken($value)
+	{
+	    $this->remember_token = $value;
+	}
+
+	public function getRememberTokenName()
+	{
+	    return 'remember_token';
 	}
 
 }
